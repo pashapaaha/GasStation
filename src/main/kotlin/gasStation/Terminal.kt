87ebd0088983ad62ca34330c038f0e449e3d9656
Pavel Andreyev <paaha98@gmail.com>
2020-01-05 12:21:22 +0300

    Добавлен класс состояний

--- src/main/kotlin/gasStation/Terminal.kt
+++ src/main/kotlin/gasStation/Terminal.kt
@@ -1,20 +1,25 @@
 package gasStation
 
 class Terminal(
-    val capacity: Int,
+    private val capacity: Int,
     val gasType: GasType,
+    var state: TerminalState = TerminalState.WORKING,
     var fill: Int = capacity
 ) {
     val minFill: Int = capacity / 10
 
     fun service(car: Car): Boolean {
-
+        println("\nTerminal: service(). start service ~~~~~~~~~~~~~~~~~~~~~~~")
         if(car.request > fill) {
+            println("Terminal: service(). Gas is not enough")
             return false
         }
 
+        println("Terminal: service(). Old fill is $fill")
         fill -= car.request
+        println("Terminal: service(). New fill is $fill")
 
+        println("Terminal: service(). Finish service ~~~~~~~~~~~~~~~~~~~~~~~\n")
         return true
     }
 }
