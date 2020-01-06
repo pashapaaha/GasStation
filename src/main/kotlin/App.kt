c52795106a8c14333203d44eaf578a2757bbcae6
Pavel Andreyev <paaha98@gmail.com>
2020-01-06 17:43:22 +0300

    Работа с консольным вводом-выводом вынесена в отдельный класс

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,37 +1,5 @@
-import gasStation.Car
-import gasStation.GasType
-import gasStation.Terminal
+import console.ConsoleIO
 
 fun main() {
-    val terminals = GasType.values().map { Terminal(1000, it) }
-    while (true) {
-        val car = createCar()
-        if (car == null) {
-            println(".main(): Водитель, пораженный своим невежеством в типах топлива, уехал в слезах")
-            continue
-        }
-        val isSuccess = terminals.find { car.gasType == it.gasType }!!.service(car)
-        if (isSuccess) {
-            println("Обслуживание завершилось успешно\n~~~~~~~~~~~~~~~~")
-        } else {
-            println("Морда клиента очень грустная\n~~~~~~~~~~~~~~~~")
-        }
-    }
-}
-
-fun createCar(): Car? {
-    println("###")
-    val type = inputInt("Тип топлива: ")
-    val request = inputInt("Количество:  ")
-    val gasType = GasType.values().find { it.octane == type } ?: return null
-
-    return Car(gasType, request)
-}
-
-fun inputInt(title: String): Int {
-    while (true) {
-        print(title)
-        val value = readLine()?.toIntOrNull()
-        if (value != null) return value
-    }
+    ConsoleIO().startImitation()
 }
